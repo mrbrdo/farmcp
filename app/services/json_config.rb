057e56b8f4025479f30fb1602ff810d08e943254
Jan Berdajs <mrbrdo@gmail.com>
2014-03-25 16:46:30 +0100

    don't return nil for config if config file doesn't exist, return an empty hash so get[] still works

--- app/services/json_config.rb
+++ app/services/json_config.rb
@@ -6,6 +6,8 @@ module JsonConfig
       conf_file = File.expand_path("~/.farmcp")
       if File.exist?(conf_file)
         JSON.load(File.read(conf_file))
+      else
+        Hash.new
       end
     end
   end
